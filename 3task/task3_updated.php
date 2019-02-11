58c4cb06b8c4e2c270a3c8dfac5b105699258519
milmaverick <41447448+milmaverick@users.noreply.github.com>
2019-02-11 17:56:36 +0800

    3rdtsk

--- 3task/task3_updated.php
+++ 3task/task3_updated.php
@@ -1,25 +1,38 @@
 <?php
+	
+	function reflectionImage($FileName){
+		
+		list($width, $height) = getimagesize($FileName);
+		$image_p = imagecreatetruecolor($width, $height);
+		$image = imagecreatefromjpeg($FileName);
+		imagecopyresampled($image_p, $image, 0, 0, $width - 1, 0, $width, $height, - $width, $height);
+		imagecopymerge($image_p, $image, 0, 0, 0, 0, $width/2, $height, 100);
+		imagepng($image_p, "image.png", 0, -1);
+
+	}
+
 
 	function SaveImage($url) {
-	// открываем содержимое файла по ссылке
-	$Image = @file_get_contents($url);
-	if(!$Image) {
-		return "Не удалось получить данные файла по ссылке";
-	}
-	 else {
 	
-	 	$ExplodeUrl = explode("/", $url);
-		$FileName = $ExplodeUrl[count($ExplodeUrl)-1];
-		
-		if(file_exists($FileName)) {
-			$FileName = rand(10000000, 99999999).$FileName;
+		$Image = @file_get_contents($url);
+		if(!$Image) {
+			return "Не удалось получить данные файла по ссылке";
 		}
+		 else {
 		
-		$CreateNewFile = fopen($FileName, "w+");
-		$WriteInFile = fwrite($CreateNewFile, $Image);
-		return $FileName;
-	 }
-}
-echo Saveimage("http://live-code.ru/uploads/twitter.png");
+		 	$ExplodeUrl = explode("/", $url);
+			$FileName = $ExplodeUrl[count($ExplodeUrl)-1];
+			
+			if(file_exists($FileName)) {
+				$FileName = rand(10000000, 99999999).$FileName;
+			}
+			
+			$CreateNewFile = fopen($FileName, "w+");
+			$WriteInFile = fwrite($CreateNewFile, $Image);
+			return $FileName;
+		 }
+	}
 
-?>
+	reflectionImage(SaveImage("https://www.nastol.com.ua/large/201110/9034.jpg"));
+
+	?>
